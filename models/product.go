ee0d54d971737ed1a596845ac93922e70641994a
Pushpanjali07 <125995319+Pushpanjali07@users.noreply.github.com>
2025-05-26 22:47:56 +0530

    Update product.go

--- models/product.go
+++ models/product.go
@@ -3,7 +3,7 @@ package models
 // We import GORM to use its built-in model fields and ORM features
 
 type Product struct {
-	ID          uint    `gorm:"primaryKey"`
+	ID          uint    `json:"id" gorm:"primaryKey"`
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
