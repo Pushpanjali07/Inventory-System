f39bf80a9fc65dd0d3359a732fc3b9c2196c72d1
Pushpanjali07 <125995319+Pushpanjali07@users.noreply.github.com>
2025-05-26 22:48:38 +0530

    Update Inventory_Retrieval.go

--- controllers/Inventory_Retrieval.go
+++ controllers/Inventory_Retrieval.go
@@ -9,6 +9,6 @@ import (
 
 func GetInventory(c *gin.Context) {
 	var products []models.Product
-	models.DB.Select("id", "name", "quantity").Find(&products)
+	models.DB.Select("id","description","price", "name", "quantity").Find(&products)
 	c.JSON(http.StatusOK, products)
 }
