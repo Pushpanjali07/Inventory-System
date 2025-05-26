d35b445c1500d16431907e100c622b37ce5d6dbd
Pushpanjali07 <125995319+Pushpanjali07@users.noreply.github.com>
2025-05-26 22:14:34 +0530

    Update db.go

--- models/db.go
+++ models/db.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB // global variable to access db connection anywhere
 
 func ConnectDatabase() {
-	dsn := "root:Mohit@123@tcp(127.0.0.1:3306)/inventory_db?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:password@tcp(127.0.0.1:3306)/inventory_db?charset=utf8mb4&parseTime=True&loc=Local"
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}) //Open a connection to the database using GORM
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
